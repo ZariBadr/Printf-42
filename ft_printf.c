236e4f904da0757e0c16ac0fb9053213c4786f00
BADREDDINE ZARI <113315895+ZariBadr@users.noreply.github.com>
2025-11-17 21:36:28 +0100

    main function - printf

---
+++ ft_printf.c
@@ -0,0 +1,40 @@
+#include <stdarg.h>
+#include <stdint.h>
+
+int ft_convert(const char str,va_list args)
+{
+    int nprinted;
+    nprinted = 0;
+
+    if (str == 'c')
+        nprinted += ft_putchar(va_arg(args,int));
+    else if (str == 's')
+        nprinted += ft_putstr(va_arg(args,char *));
+    else if (str == 'p')
+        nprinted += ft_putptr(va_arg(args,uintptr_t));
+
+}
+
+
+int ft_printf(const char * str, ...)
+{
+    int i;
+    int nprinted;
+    va_list args;
+    va_start(args,str);
+
+    i = 0;
+    nprinted = 0;
+    while (str[i])
+    {
+        if (str[i] == '%')
+        {
+            nprinted += ft_convert(str[i],args);
+            i++;
+        }
+        else
+        {
+            nprinted += ft_putchar(str[i]);
+        }
+    }
+}
